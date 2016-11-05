39244fe9f2cc4016b349476bea29bf1058aececd
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-05 17:29:21 +0700

    Refactor abstract base Bicycle WIP

--- ch6-inheritance/bicycle.rb
+++ ch6-inheritance/bicycle.rb
@@ -1,12 +1,30 @@
 class Bicycle
-  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
+  attr_reader :size, :chain, :tire_size
+
+  def initalize(*args)
+    @size      = args[:size]
+    @chain     = args[:chain]     || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+  end
+
+  def default_chain
+    '10-speed'
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :style, :tape_color, :front_shock, :rear_shock
 
   def initialize(*args)
     @style = args[:style]
-    @size = args[:size]
     @tape_color = args[:tape_color]
     @front_shock = args[:front_shock]
     @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def default_tire_size
+    '23'
   end
 
   # checking style starts down a slippery slope
@@ -37,18 +55,26 @@ class MountainBike < Bicycle
     super(args)
   end
 
+  def default_tire_size
+    '2.1'
+  end
+
   def spares
     super.merge(rear_shock: rear_shock)
   end
 end
 
-mountain_bike = MountainBike.new( size: 'S',
-                                 front_shock:  'Manitou',
-                                 rear_shock:   'Fox')
-mountain_bike.size # -> 'S'
-mountain_bike.spares
-# -> {:tire_size   => "23",       <- wrong!
-#     :chain       => "10-speed",
-#     :tape_color  => nil,        <- not applicable
-#     :front_shock => 'Manitou',
-#     :rear_shock  => "Fox"}
+road_bike = RoadBike.new(
+  size:       'M',
+  tape_color: 'red'
+)
+
+road_bike.tire_size     # => '23'
+road_bike.chain         # => "10-speed"
+mountain_bike = MountainBike.new(
+  size:        'S',
+  front_shock: 'Manitou',
+  rear_shock:  'Fox'
+)
+mountain_bike.tire_size # => '2.1'
+road_bike.chain         # => "10-speed"

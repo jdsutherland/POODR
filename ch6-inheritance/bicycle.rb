c6e583934a259169dd14a25d9a308bf2e61804a3
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-05 16:25:10 +0700

    Add mountain bike functionality to Bicycle

--- ch6-inheritance/bicycle.rb
+++ ch6-inheritance/bicycle.rb
@@ -1,23 +1,38 @@
 class Bicycle
+  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
+
   def initialize(*args)
+    @style = args[:style]
     @size = args[:size]
     @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
+  # checking style starts down a slippery slope
   def spares
-    {
-      chain:      '10 speed',
-      tire_size:  '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      {
+        chain:      '10 speed',
+        tire_size:  '23', # millimeters
+        tape_color: tape_color
+      }
+    else
+      {
+        chain:      '10 speed',
+        tire_size:  '2.1', # inches
+        rear_shock: rear_shock
+      }
+    end
   end
 
   # Many other methods...
 end
 
 bike = Bicycle.new(
-  size: 'M',
-  tape_color: 'red'
+  style: :mountain,
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox'
 )
-bize.size
 bize.spares

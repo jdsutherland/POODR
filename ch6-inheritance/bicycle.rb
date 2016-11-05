6e0b0876a35fcddc7f09e94df3c4051bcfaa461d
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-05 16:40:45 +0700

    Add initial MountainBike subclass to show bad

--- ch6-inheritance/bicycle.rb
+++ ch6-inheritance/bicycle.rb
@@ -25,14 +25,30 @@ class Bicycle
       }
     end
   end
-
-  # Many other methods...
 end
 
-bike = Bicycle.new(
-  style: :mountain,
-  size: 'S',
-  front_shock: 'Manitou',
-  rear_shock: 'Fox'
-)
-bize.spares
+# Initial attempt at MountainBike subclass (it's bad)
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(*args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+mountain_bike = MountainBike.new( size: 'S',
+                                 front_shock:  'Manitou',
+                                 rear_shock:   'Fox')
+mountain_bike.size # -> 'S'
+mountain_bike.spares
+# -> {:tire_size   => "23",       <- wrong!
+#     :chain       => "10-speed",
+#     :tape_color  => nil,        <- not applicable
+#     :front_shock => 'Manitou',
+#     :rear_shock  => "Fox"}

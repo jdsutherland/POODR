a03ace9b7684cb07c96c26ee04298ee14cb55325
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-08 23:12:25 +0700

    Add initial command message test using mocks
    Gear modified with the new example scenario of a game where players
    race virtual bicycles; Gear is now responsible for letting the
    application know when the player changes gears so it can update the
    bicycle's behavior.

--- ch9-testing/ch3_test.rb
+++ ch9-testing/ch3_test.rb
@@ -19,6 +19,29 @@ class WheelTest < MiniTest::Unit::TestCase
 end
 
 class GearTest < MiniTest::Unit::TestCase
+  def setup
+    @observer = Minitest::Mock.new
+
+    @gear = Gear.new(
+      chainring: 52,
+      cog:       11,
+      wheel:     DiameterDouble.new,
+      observer:  @observer
+    )
+  end
+
+  def test_notifies_observers_when_cogs_change
+    @observer.expect(:changed, true, [52, 27])
+    @gear.set_cog(27)
+    @observer.verify
+  end
+
+  def test_notifies_observers_when_chainrings_change
+    @observer.expect(:changed, true, [42, 11])
+    @gear.set_chainring(42)
+    @observer.verify
+  end
+
   def test_calculates_gear_inches
     gear = Gear.new(
       chainring: 52,

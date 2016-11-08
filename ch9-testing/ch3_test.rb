46d03d76b89d36e1e261d3592badb3c9d5ac8bb6
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-08 20:49:59 +0700

    Add gear test
    The gear test demonstrates hidden coupling between Wheel. If Wheel were
    expensive to create, we pay a cost even though we aren't interested in
    wheel. If Gear is correct but Wheel is broken, the test might fail in
    a misleading way.

--- ch9-testing/ch3_test.rb
+++ ch9-testing/ch3_test.rb
@@ -9,3 +9,16 @@ class WheelTest < MiniTest::Unit::TestCase
     assert_in_delta(29, wheel.diameter, 0.01)
   end
 end
+
+class GearTest < MiniTest::Unit::TestCase
+  def test_calculates_gear_inches
+    gear = Gear.new(
+      chainring: 52,
+      cog:       11,
+      rim:       26,
+      tire:      1.5
+    )
+
+    assert_in_delta(137.1, gear.gear_inches, 0.01)
+  end
+end

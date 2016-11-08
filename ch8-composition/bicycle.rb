fb1fc1f80a57eca3d91fcf6fcaa0f512b642a25d
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-08 11:43:12 +0700

    Add initial PartsFactory

--- ch8-composition/bicycle.rb
+++ ch8-composition/bicycle.rb
@@ -1,5 +1,6 @@
 require_relative 'part'
 require_relative 'parts'
+require_relative 'parts_factory'
 
 class Bicycle
   attr_reader :size, :parts
@@ -47,3 +48,14 @@ class MountainBikeParts < Parts
     { rear_shock: rear_shock }
   end
 end
+
+road_config =
+  [['chain',        '10-speed'],
+   ['tire_size',    '23'],
+   ['tape_color',   'red']]
+mountain_config =
+  [['chain',        '10-speed'],
+   ['tire_size',    '2.1'],
+   ['front_shock',  'Manitou', false],
+   ['rear_shock',   'Fox']]
+
---
+++ ch8-composition/parts_factory.rb
@@ -0,0 +1,12 @@
+class PartsFactory
+  def self.build(config, part_class = Part, parts_class = Parts)
+    parts_class.new(
+      config.map do |part_config|
+        part_class.new(
+          name:        part_config[0],
+          description: part_config[1],
+          needs_spare: part_config.fetch(2, true))
+      end
+    )
+  end
+end

c12fb837f39a30818fb56eabae17d4cdcff43a2d
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-08 11:57:25 +0700

    Refactor factory to make Part OpenStruct

--- ch8-composition/parts_factory.rb
+++ ch8-composition/parts_factory.rb
@@ -1,12 +1,15 @@
-class PartsFactory
+require 'ostruct'
+
+module PartsFactory
   def self.build(config, part_class = Part, parts_class = Parts)
     parts_class.new(
-      config.map do |part_config|
-        part_class.new(
-          name:        part_config[0],
-          description: part_config[1],
-          needs_spare: part_config.fetch(2, true))
-      end
-    )
+      config.map { |part_config| create_part(part_config) })
+  end
+
+  def self.create_part(part_config)
+    OpenStruct.new(
+      name:        part_config[0],
+      description: part_config[1],
+      needs_spare: part_config.fetch(2, true))
   end
 end

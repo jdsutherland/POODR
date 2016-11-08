bc70bd22e91afd74aeff068d09c46b7de4e1985e
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-08 22:50:57 +0700

    Add test to document role
    This is an example of a way to raise a role's visibility, but it is
    inadequate. It will be improved in the future.

--- ch9-testing/ch3_test.rb
+++ ch9-testing/ch3_test.rb
@@ -3,6 +3,14 @@ require 'minitest/autorun'
 require_relative 'ch3'
 
 class WheelTest < MiniTest::Unit::TestCase
+  def setup
+    @wheel = Wheel.new(26, 1.5)
+  end
+
+  def test_implements_the_diameterizable_interface
+    assert_respond_to(@wheel, :diameter)
+  end
+
   def test_calculates_diameter
     wheel = Wheel.new(26, 1.5)
 

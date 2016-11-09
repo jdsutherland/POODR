1fb38c4102b80d8ade74b64df9887251a29a5834
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-09 14:37:29 +0700

    Add confirming subclass behavior tests

--- ch9-testing/bicycle_interface_test.rb
+++ ch9-testing/bicycle_interface_test.rb
@@ -1,23 +1,21 @@
-require 'ch6'
-
 module BicycleInterfaceTest
   def test_responds_to_default_chain
-    assert_responds_to(@object, :default_chain)
+    assert_respond_to(@object, :default_chain)
   end
 
   def test_responds_to_default_tire_size
-    assert_responds_to(@object, :default_tire_size)
+    assert_respond_to(@object, :default_tire_size)
   end
 
   def test_responds_to_spares
-    assert_responds_to(@object, :spares)
+    assert_respond_to(@object, :spares)
   end
 
   def test_responds_to_chain
-    assert_responds_to(@object, :chain)
+    assert_respond_to(@object, :chain)
   end
 
   def test_responds_to_tire_size
-    assert_responds_to(@object, :tire_size)
+    assert_respond_to(@object, :tire_size)
   end
 end
--- ch9-testing/ch6_test.rb
+++ ch9-testing/ch6_test.rb
@@ -2,6 +2,30 @@ require 'minitest/autorun'
 
 require_relative 'ch6'
 require_relative 'bicycle_interface_test'
+require_relative 'bicycle_subclass_test'
 
 class BicycleTest < MiniTest::Unit::TestCase
+  include BicycleInterfaceTest
+
+  def setup
+    @bike = @object = Bicycle.new(tire_size: 0)
+  end
+end
+
+class RoadBikeTest < MiniTest::Unit::TestCase
+  include BicycleInterfaceTest
+  include BicycleSubclassTest
+
+  def setup
+    @bike = @object = RoadBike.new
+  end
+end
+
+class MountainBikeTest < MiniTest::Unit::TestCase
+  include BicycleInterfaceTest
+  include BicycleSubclassTest
+
+  def setup
+    @bike = @object = MountainBike.new
+  end
 end

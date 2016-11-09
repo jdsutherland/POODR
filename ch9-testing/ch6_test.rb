f5fad56b430e177f7d06bab4c390751655ae844c
Jeff Sutherland <jeffdsuth@gmail.com>
2016-11-09 14:57:14 +0700

    Add abstract superclass test
    We create a stub for Bicycle since it's an abstract superclass.

--- ch9-testing/ch6_test.rb
+++ ch9-testing/ch6_test.rb
@@ -4,15 +4,35 @@ require_relative 'ch6'
 require_relative 'bicycle_interface_test'
 require_relative 'bicycle_subclass_test'
 
+class StubbedBike < Bicycle
+  def default_tire_size
+    0
+  end
+
+  def local_spares
+    { saddle: 'painful' }
+  end
+end
+
 class BicycleTest < MiniTest::Unit::TestCase
   include BicycleInterfaceTest
 
   def setup
     @bike = @object = Bicycle.new(tire_size: 0)
+    @stubbed_bike = StubbedBike.new
   end
 
   def test_forces_subclasses_to_implement_default_tire_size
     assert_raises(NotImplementedError) { @bike.default_tire_size }
+  end
+
+  def test_includes_local_spares_in_spares
+    assert_equal @stubbed_bike.spares,
+    {
+      tire_size: 0,
+      chain:     '10-speed',
+      saddle:    'painful'
+    }
   end
 end
 

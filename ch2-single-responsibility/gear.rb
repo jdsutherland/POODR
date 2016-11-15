04876e4114ec4ad07ce328aad1da5186866e094d
Jeff Sutherland <jdsuther@asu.edu>
2016-11-15 22:57:45 +0700

    Add completed ch2 wheel

--- ch2-single-responsibility/gear.rb
+++ ch2-single-responsibility/gear.rb
@@ -1,7 +1,7 @@
 class Gear
   attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel = nil)
     @chainring = chainring
     @cog       = cog
     @wheel     = Wheel.new(rim, tire)
@@ -12,12 +12,28 @@ class Gear
   end
 
   def add_gear_inches
-    ratio * diameter
-  end
-
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim * (tire * 2)
-    end
+    ratio * wheel.diameter
   end
 end
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim * (tire * 2)
+  end
+
+  def circumference
+    diameter * Math::PI
+  end
+end
+
+@wheel = Wheel.new(26, 1.5)
+puts @wheel.circumference # -> 91.106186954104
+puts Gear.new(52, 11, @wheel).gear_inches # -> 137.090909090909
+puts Gear.new(52, 11).ratio # -> 4.72727272727273
